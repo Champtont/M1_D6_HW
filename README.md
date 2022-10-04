71e357a718d7fc486a2d609389837138493c7031
Courtney Hampton <jaye0888@gmail.com>
2022-10-04 13:48:18 +0200

    Homework
    html and Js attempt

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # M1_D6_HW
+
 Homework for Day six module one
+Playing with JS
---
+++ index.js
@@ -0,0 +1,13 @@
+let firstName = "Courtney ";
+let middleName = "Jaye ";
+let lastName = "Hampton-Thomas";
+let fullName = firstName + middleName + lastName;
+console.log("hello, my name is", fullName);
+let myAge = 34;
+let currentYear = 2022;
+let myBirthYear = currentYear - myAge;
+console.log("I was born in", myBirthYear);
+document.getElementById("name").innerHTML =
+  "Hello, my name is " + fullName + "!";
+document.getElementById("year").innerHTML =
+  "I was born in " + myBirthYear + ".";
